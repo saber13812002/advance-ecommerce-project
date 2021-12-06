832f485eeedd23ff6663865667e08d4a0a7a08b1
saber <saber.tabatabaee@gmail.com>
2021-12-06 23:56:43 +0330

    fix: bug coupon view admin page

--- resources/views/backend/coupon/view_coupon.blade.php
+++ resources/views/backend/coupon/view_coupon.blade.php
@@ -2,154 +2,155 @@
 @section('admin')
 
 
-  <!-- Content Wrapper. Contains page content -->
-  
-	  <div class="container-full">
-		<!-- Content Header (Page header) -->
-		 
+    <!-- Content Wrapper. Contains page content -->
 
-		<!-- Main content -->
-		<section class="content">
-		  <div class="row">
-			   
-		 
-
-			<div class="col-8">
-
-			 <div class="box">
-				<div class="box-header with-border">
-				  <h3 class="box-title">{{ trans("admin.Coupon List")   }} <span class="badge badge-pill badge-danger"> {{ count($coupons) }} </span></h3>
-				</div>
-				<!-- /.box-header -->
-				<div class="box-body">
-					<div class="table-responsive">
-					  <table id="example1" class="table table-bordered table-striped">
-						<thead>
-							<tr>
-								<th>{{ trans("admin.Coupon Name")   }} </th>
-								<th>{{ trans("admin.Coupon Discount")   }}</th>
-								<th>{{ trans("admin.Validity")   }} </th>
-								<th>{{ trans("admin.Status")   }} </th>
-								<th>{{ trans("admin.Action")   }}</th>
-								 
-							</tr>
-						</thead>
-						<tbody>
-	 @foreach($coupons as $item)
-	 <tr>
-		<td> {{ $item->coupon_name }}  </td>
-		<td> {{ $item->coupon_discount }}% </td>
-		<td width="25%">
-       {{ Carbon\Carbon::parse($item->coupon_validity)->format('D, d F Y') }}
-			 </td>
-
-		<td>
-		 	@if($item->coupon_validity >= Carbon\Carbon::now()->format('Y-m-d'))
-		 	<span class="badge badge-pill badge-success"> Valid </span>
-		 	@else
-           <span class="badge badge-pill badge-danger"> Invalid </span>
-		 	@endif
-
-		 </td>
-
-		<td width="25%">
- <a href="{{ route('coupon.edit',$item->id) }}" class="btn btn-info" title="Edit Data"><i class="fa fa-pencil"></i> </a>
- <a href="{{ route('coupon.delete',$item->id) }}" class="btn btn-danger" title="Delete Data" id="delete">
- 	<i class="fa fa-trash"></i></a>
-		</td>
-							 
-	 </tr>
-	  @endforeach
-						</tbody>
-						 
-					  </table>
-					</div>
-				</div>
-				<!-- /.box-body -->
-			  </div>
-			  <!-- /.box -->
-
-			          
-			</div>
-			<!-- /.col -->
+    <div class="container-full">
+        <!-- Content Header (Page header) -->
 
 
-<!--   ------------ Add Category Page -------- -->
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
 
 
-          <div class="col-4">
+                <div class="col-8">
 
-			 <div class="box">
-				<div class="box-header with-border">
-				  <h3 class="box-title">{{ trans("admin.Add Coupon")   }} </h3>
-				</div>
-				<!-- /.box-header -->
-				<div class="box-body">
-					<div class="table-responsive">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">{{ trans("admin.Coupon List")   }} <span
+                                    class="badge badge-pill badge-danger"> {{ count($coupons) }} </span></h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>{{ trans("admin.Coupon Name")   }} </th>
+                                        <th>{{ trans("admin.Coupon Discount")   }}</th>
+                                        <th>{{ trans("admin.Validity")   }} </th>
+                                        <th>{{ trans("admin.Status")   }} </th>
+                                        <th>{{ trans("admin.Action")   }}</th>
 
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($coupons as $item)
+                                        <tr>
+                                            <td> {{ $item->coupon_name }}  </td>
+                                            <td> {{ $item->coupon_discount }}%</td>
+                                            <td width="25%">
+                                                {{ Carbon\Carbon::parse($item->coupon_validity)->format('D, d F Y') }}
+                                            </td>
 
- <form method="post" action="{{ route('coupon.store') }}" >
-	 	@csrf
-					   
+                                            <td>
+                                                @if($item->coupon_validity >= Carbon\Carbon::now()->format('Y-m-d'))
+                                                    <span class="badge badge-pill badge-success"> Valid </span>
+                                                @else
+                                                    <span class="badge badge-pill badge-danger"> Invalid </span>
+                                                @endif
 
-	 <div class="form-group">
-		<h5>{{ trans("admin.Coupon Name")   }}  <span class="text-danger">*</span></h5>
-		<div class="controls">
-	 <input type="text"  name="coupon_name" class="form-control" > 
-	 @error('coupon_name') 
-	 <span class="text-danger">{{ $message }}</span>
-	 @enderror 
-	</div>
-	</div>
+                                            </td>
 
+                                            <td width="25%">
+                                                <a href="{{ route('coupon.edit',$item->id) }}" class="btn btn-info"
+                                                   title="Edit Data"><i class="fa fa-pencil"></i> </a>
+                                                <a href="{{ route('coupon.delete',$item->id) }}" class="btn btn-danger"
+                                                   title="Delete Data" id="delete">
+                                                    <i class="fa fa-trash"></i></a>
+                                            </td>
 
-	<div class="form-group">
-		<h5>{{ trans("admin.Coupon Discount(%)")   }} <span class="text-danger">*</span></h5>
-		<div class="controls">
-	 <input type="text" name="coupon_discount" class="form-control" >
-	 <span class="text-danger">{{ $message }}</span>
-	 @enderror 
-	  </div>
-	</div>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
 
-
-	<div class="form-group">
-		<h5>{{ trans("admin.Coupon Validity Date")   }}  <span class="text-danger">*</span></h5>
-		<div class="controls">
-	 <input type="date" name="coupon_validity" class="form-control" min="{{ Carbon\Carbon::now()->format('Y-m-d') }}">
-     @error('coupon_validity') 
-	 <span class="text-danger">{{ $message }}</span>
-	 @enderror 
-	  </div>
-	</div> 
-					 
-
-			 <div class="text-xs-right">
-	<input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add New">					 
-						</div>
-					</form>
-
-
-
-
-					  
-					</div>
-				</div>
-				<!-- /.box-body -->
-			  </div>
-			  <!-- /.box --> 
-			</div>
-
- 
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
 
 
-		  </div>
-		  <!-- /.row -->
-		</section>
-		<!-- /.content -->
-	  
-	  </div>
-  
+                </div>
+                <!-- /.col -->
+
+
+                <!--   ------------ Add Category Page -------- -->
+
+
+                <div class="col-4">
+
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">{{ trans("admin.Add Coupon")   }} </h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+
+
+                                <form method="post" action="{{ route('coupon.store') }}">
+                                    @csrf
+
+
+                                    <div class="form-group">
+                                        <h5>{{ trans("admin.Coupon Name")   }} <span class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="text" name="coupon_name" class="form-control">
+                                            @error('coupon_name')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <h5>{{ trans("admin.Coupon Discount(%)")   }} <span class="text-danger">*</span>
+                                        </h5>
+                                        <div class="controls">
+                                            <input type="text" name="coupon_discount" class="form-control">
+                                            @error('coupon_name')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group">
+                                        <h5>{{ trans("admin.Coupon Validity Date")   }} <span
+                                                class="text-danger">*</span></h5>
+                                        <div class="controls">
+                                            <input type="date" name="coupon_validity" class="form-control"
+                                                   min="{{ Carbon\Carbon::now()->format('Y-m-d') }}">
+                                            @error('coupon_validity')
+                                            <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+
+                                    <div class="text-xs-right">
+                                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add New">
+                                    </div>
+                                </form>
+
+
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+
+
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+
+    </div>
+
 
 
 
