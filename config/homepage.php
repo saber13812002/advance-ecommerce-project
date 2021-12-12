6f4607359ea7d2a83cf1f63c458df0a0d6be4625
saber13812002 <saber.tabatabaee@gmail.com>
2021-12-12 11:46:50 +0330

    feat: homepage full initialization

--- config/homepage.php
+++ config/homepage.php
@@ -12,22 +12,44 @@ return [
         'icon_url' => '/storage/upload/icons/checklist.svg',
         'title' => 'سفارشات من',
         'action_type' => 'deep_link',
-        'action' => 'shop/myorders'
-    ],
-    [
-        'style_type' => 'banner',
-        'key' => 'id',
-        'value' => 2,
-        'title' => 'رژیم+ورزش',
-        'action_type' => 'link',
-        'action' => 'https://app.fitamin.ir/'
+        'action' => 'shop/orders'
     ],
     [
         'style_type' => 'product_category',
         'category_id' => 1,
         'product_ids' => [1, 2],
         'action_type' => 'deep_link',
-        'action' => 'shop/category'
+        'action' => 'shop/category/1'
+    ],
+    [
+        'style_type' => 'banner',
+        'key' => 'id',
+        'value' => 2,
+        'title' => 'رژیم دکتر کرمانی',
+        'action_type' => 'link',
+        'action' => 'https://kermany.com/bmi/'
+    ],
+    [
+        'style_type' => 'product_category',
+        'category_id' => 3,
+        'product_ids' => [3, 4],
+        'action_type' => 'deep_link',
+        'action' => 'shop/category/3'
+    ],
+    [
+        'style_type' => 'banner',
+        'key' => 'id',
+        'value' => 4,
+        'title' => 'روانشناسی',
+        'action_type' => 'deep_link',
+        'action' => '/psy/intro'
+    ],
+    [
+        'style_type' => 'product_category',
+        'category_id' => 2,
+        'product_ids' => [1, 3],
+        'action_type' => 'deep_link',
+        'action' => 'shop/category/2'
     ],
     [
         'style_type' => 'banner',
@@ -36,20 +58,5 @@ return [
         'title' => 'فیتامین',
         'action_type' => 'link',
         'action' => 'https://app.fitamin.ir/'
-    ],
-    [
-        'style_type' => 'product_category',
-        'category_id' => 3,
-        'product_ids' => [3, 4],
-        'action_type' => 'deep_link',
-        'action' => 'shop/category'
-    ],
-    [
-        'style_type' => 'banner',
-        'key' => 'id',
-        'value' => 4,
-        'title' => 'روانشناسی',
-        'action_type' => 'link',
-        'action' => 'https://psychology.kermany.ir/'
     ],
 ];
