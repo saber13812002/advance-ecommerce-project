543ebe8c45bd75f7f56eee77f37501f922e25724
saber <saber.tabatabaee@gmail.com>
2021-12-14 23:33:15 +0330

    fix: upload image product

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -147,7 +147,7 @@ class ProductController extends Controller
 
 
         $image = $request->file('product_thambnail');
-        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        $name_gen = date('Y-m-d-H:i:s') . hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
         Image::make($image)->resize(917, 1000)->save('storage/upload/products/thambnail/' . $name_gen);
         $save_url = 'storage/upload/products/thambnail/' . $name_gen;
 
