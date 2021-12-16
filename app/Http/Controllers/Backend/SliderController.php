22b5920690f1b0f04f1f3d3c14d056c24920879b
saber <saber.tabatabaee@gmail.com>
2021-12-16 23:38:21 +0330

    fix: slider upload problem

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -57,8 +57,8 @@ class SliderController extends Controller
         ]);
 
         $image = $request->file('slider_img');
-        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-        Image::make($image)->resize(870, 370)->save('storage/upload/slider/' . $name_gen);
+        $name_gen = date('Y-m-d-H-i-s') . hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(870, 370)->save(public_path('/upload/slider/') . $name_gen);
         $save_url = 'storage/upload/slider/' . $name_gen;
 
         Slider::insert([
