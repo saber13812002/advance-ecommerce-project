2947f5cbcb3bd2254633299f8f2d5f6d903ce673
saber <saber.tabatabaee@gmail.com>
2021-12-17 19:08:03 +0330

    feat: some code cleanup

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function ()
 
 Route::middleware(['auth:admin'])->group(function () {
 
-
     Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
         return view('admin.index');
     })->name('dashboard')->middleware('auth:admin');
@@ -66,15 +65,10 @@ Route::middleware(['auth:admin'])->group(function () {
 // Admin All Routes
 
     Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
-
     Route::get('/admin/profile', [AdminProfileController::class, 'AdminProfile'])->name('admin.profile');
-
     Route::get('/admin/profile/edit', [AdminProfileController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
-
     Route::post('/admin/profile/store', [AdminProfileController::class, 'AdminProfileStore'])->name('admin.profile.store');
-
     Route::get('/admin/change/password', [AdminProfileController::class, 'AdminChangePassword'])->name('admin.change.password');
-
     Route::post('/update/change/password', [AdminProfileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
 
 });  // end Middleware admin
@@ -89,13 +83,9 @@ Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function 
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
-
 Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
-
 Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
-
 Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password');
-
 Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
 
 
@@ -103,15 +93,10 @@ Route::middleware(['auth:admin'])->prefix('admin')->group(function () {
 // Admin Brand All Routes
 
     Route::prefix('brand')->group(function () {
-
         Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand');
-
         Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
-
         Route::get('/edit/{id}', [BrandController::class, 'BrandEdit'])->name('brand.edit');
-
         Route::post('/update', [BrandController::class, 'BrandUpdate'])->name('brand.update');
-
         Route::get('/delete/{id}', [BrandController::class, 'BrandDelete'])->name('brand.delete');
 
     });
@@ -120,42 +105,28 @@ Route::middleware(['auth:admin'])->prefix('admin')->group(function () {
     Route::prefix('category')->group(function () {
 
         Route::get('/view', [CategoryController::class, 'CategoryView'])->name('all.category');
-
         Route::post('/store', [CategoryController::class, 'CategoryStore'])->name('category.store');
-
         Route::get('/edit/{id}', [CategoryController::class, 'CategoryEdit'])->name('category.edit');
-
         Route::post('/update/{id}', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
-
         Route::get('/delete/{id}', [CategoryController::class, 'CategoryDelete'])->name('category.delete');
 
 // Admin Sub Category All Routes
 
         Route::get('/sub/view', [SubCategoryController::class, 'SubCategoryView'])->name('all.subcategory');
-
         Route::post('/sub/store', [SubCategoryController::class, 'SubCategoryStore'])->name('subcategory.store');
-
         Route::get('/sub/edit/{id}', [SubCategoryController::class, 'SubCategoryEdit'])->name('subcategory.edit');
-
         Route::post('/update', [SubCategoryController::class, 'SubCategoryUpdate'])->name('subcategory.update');
-
         Route::get('/sub/delete/{id}', [SubCategoryController::class, 'SubCategoryDelete'])->name('subcategory.delete');
 
 
 // Admin Sub->Sub Category All Routes
 
         Route::get('/sub/sub/view', [SubCategoryController::class, 'SubSubCategoryView'])->name('all.subsubcategory');
-
         Route::get('/subcategory/ajax/{category_id}', [SubCategoryController::class, 'GetSubCategory']);
-
         Route::get('/sub-subcategory/ajax/{subcategory_id}', [SubCategoryController::class, 'GetSubSubCategory']);
-
         Route::post('/sub/sub/store', [SubCategoryController::class, 'SubSubCategoryStore'])->name('subsubcategory.store');
-
         Route::get('/sub/sub/edit/{id}', [SubCategoryController::class, 'SubSubCategoryEdit'])->name('subsubcategory.edit');
-
         Route::post('/sub/update', [SubCategoryController::class, 'SubSubCategoryUpdate'])->name('subsubcategory.update');
-
         Route::get('/sub/sub/delete/{id}', [SubCategoryController::class, 'SubSubCategoryDelete'])->name('subsubcategory.delete');
 
     });
@@ -177,19 +148,12 @@ Route::middleware(['auth:admin'])->prefix('admin')->group(function () {
 
 
         Route::get('/edit/{id}', [ProductController::class, 'EditProduct'])->name('product.edit');
-
         Route::post('/data/update', [ProductController::class, 'ProductDataUpdate'])->name('product-update');
-
         Route::post('/image/update', [ProductController::class, 'MultiImageUpdate'])->name('update-product-image');
-
         Route::post('/thambnail/update', [ProductController::class, 'ThambnailImageUpdate'])->name('update-product-thambnail');
-
         Route::get('/multiimg/delete/{id}', [ProductController::class, 'MultiImageDelete'])->name('product.multiimg.delete');
-
         Route::get('/inactive/{id}', [ProductController::class, 'ProductInactive'])->name('product.inactive');
-
         Route::get('/active/{id}', [ProductController::class, 'ProductActive'])->name('product.active');
-
         Route::get('/delete/{id}', [ProductController::class, 'ProductDelete'])->name('product.delete');
 
 
